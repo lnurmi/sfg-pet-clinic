59c7503391e4d3516136535030156f21614187a6
Lassi Nurmi <lassi.nurmi@vincit.fi>
2021-02-12 13:22:59 +0200

    Impelment Spring data jpa for Vet

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetServiceSDJpa.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetServiceSDJpa implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceSDJpa(final VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(final Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(final Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(final Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        vetRepository.deleteById(id);
+    }
+}
