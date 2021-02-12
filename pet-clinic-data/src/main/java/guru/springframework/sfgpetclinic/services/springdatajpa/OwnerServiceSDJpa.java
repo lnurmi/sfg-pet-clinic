5ae3494a3d0f9be430872b9de48d62ae3ee3d1f0
Lassi Nurmi <lassi.nurmi@vincit.fi>
2021-02-12 13:19:07 +0200

    Implement Spring data jpa for Owner

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerServiceSDJpa.java
@@ -0,0 +1,55 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerServiceSDJpa implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerServiceSDJpa(
+            final OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(final String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(final Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(final Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(final Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
