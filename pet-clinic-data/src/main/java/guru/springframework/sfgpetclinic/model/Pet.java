dee5cfd518964fb96fe838cff9daa9e06b8c9532
Lassi Nurmi <lassi.nurmi@vincit.fi>
2021-02-18 10:21:31 +0200

    Create or Update Pet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -36,7 +36,7 @@ public class Pet extends BaseEntity{
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
-        
+
         if (visits == null || visits.size() > 0 ) {
             this.visits = visits;
         }
